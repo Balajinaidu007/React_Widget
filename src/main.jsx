8d7f7f473c80b12469973e4ae84551b4431d1675
Balajinaidu007 <balaji.suvvada@skyronsoftware.com>
2026-04-18 23:30:44 +0530

    Adding Temp

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+let rootElement = document.getElementById("root");
+
+// If root doesn't exist (widget case), create it
+if (!rootElement) {
+  rootElement = document.createElement("div");
+  rootElement.id = "root";
+  document.body.appendChild(rootElement);
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
