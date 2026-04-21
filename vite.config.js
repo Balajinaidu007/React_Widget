ee940c469db1b7e95604f4ba01b024216e222ffe
Balajinaidu007 <balaji.suvvada@skyronsoftware.com>
2026-04-21 23:15:07 +0530

    Adding Config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: 'esnext',
+    assetsInlineLimit: 100000000, // force inline assets
     rollupOptions: {
       output: {
-        format: 'iife',   // critical for widget
-        entryFileNames: 'widget.js'
+        format: 'iife',
+        entryFileNames: 'widget.js',
+        inlineDynamicImports: true // 🔥 CRITICAL FIX
       }
     }
   }
